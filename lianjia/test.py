34e6afdc4d1e388cbf46739f1513c5a95256c71b
yawuplus <1397991131@qq.com>
2019-06-05 11:33:42 +0800

    update tree

--- lianjia/test.py
+++ lianjia/test.py
@@ -1,7 +1,8 @@
-import time
-import os
-import elasticsearch
-from config import *
-es = elasticsearch.Elasticsearch()
-if es.indices.exists(ershoufangdb()):
-    es.indices.delete(ershoufangdb())
+import re
+def switchstr(s):
+    s = s.split('-')
+    s[0] = s[0].replace('h','H')
+    s[1] = s[1].replace('w','W')
+    s = ''.join(s)
+    return s
+print(switchstr('hello-world'))
