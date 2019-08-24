650495f00d4756c435335f36964a02c1eb90b5e7
yawuplus <1397991131@qq.com>
2019-08-24 17:06:16 +0800

    mysql

---
+++ spider/dao.py
@@ -0,0 +1,8 @@
+import pymysql
+
+class Dao():
+
+    def __init__(self):
+        dbname = ''
+        username = ''
+        password = ''
--- test.py
+++ test.py
@@ -2,27 +2,12 @@ import pymysql.cursors
 
 # Connect to the database
 connection = pymysql.connect(host='localhost',
-                             user='user',
-                             password='user',
-                             db='db',
-                             charset='utf8mb4',
+                             user='root',
+                             password='root',
+                             db='lianjia',
                              cursorclass=pymysql.cursors.DictCursor)
-
-try:
-    with connection.cursor() as cursor:
-        # Create a new record
-        sql = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
-        cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
-
-    # connection is not autocommit by default. So you must commit to save
-    # your changes.
-    connection.commit()
-
-    with connection.cursor() as cursor:
-        # Read a single record
-        sql = "SELECT `id`, `password` FROM `users` WHERE `email`=%s"
-        cursor.execute(sql, ('webmaster@python.org',))
-        result = cursor.fetchone()
-        print(result)
-finally:
-    connection.close()
+cursor = connection.cursor()
+effect_row = cursor.execute('select * from users')
+# 获取剩余结果所有数据
+result = cursor.fetchall()
+print(result)
